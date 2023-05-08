1d97e3defbd78db81ec7ca47efbf306b54abd19d
fpascuales <fpascuales@gmail.com>
2023-05-08 21:03:21 +0200

    Upload image routes add

--- src/api/users/users.routes.js
+++ src/api/users/users.routes.js
@@ -1,9 +1,10 @@
 const { isAuth, isAdmin } = require("../../middlewares/auth");
+const uploadImage = require("../../middlewares/file");
 const { signUp, updateUser, deleteUser, login, getAllUsers, getUserById, checkSession } = require("./users.controller");
 
 const usersRoutes = require("express").Router();
 
-usersRoutes.post("/", signUp);
+usersRoutes.post("/", uploadImage.single('image'), signUp);
 usersRoutes.put("/:id", [isAuth], updateUser);
 usersRoutes.delete("/:id", [isAdmin], deleteUser);
 usersRoutes.post("/login", login);
